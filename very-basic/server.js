e4a00f2c87fcb67eeb8ce4554de4ed8da17b1f32
fazi1live <faziislive@gmail.com>
2022-04-22 21:01:16 +0500

    add

--- very-basic/server.js
+++ very-basic/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 7612;
+const path = require('path');
 // const Database = require('./config/DataBaseConfiguration');
 
 app.use(express.urlencoded({ extended: true }));
@@ -19,9 +20,16 @@ const _ProductManagementRoute = require('./routes/ProductManagement');
 app.use('/UserManagement',_UserManagementRoute);
 app.use('/UserAuthentication',_UserAuthenticationRoute);
 app.use('/Product',_ProductManagementRoute);
+app.use(express.static(path.join(__dirname,'/public')));
 //Initialize Routes
+
+
+//Serving Front End Form Your Server.js(Express)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+//Serving Front End Form Your Server.js(Express)
 
 app.listen(PORT,() => {
     console.log(`app is running ${PORT}`);
-    console.log(process.env);
 })
